73e4e3477c83191d0ddcab9ee60c51b81d114d2d
asikranjm <asikranjm@gmail.com>
2014-10-24 00:14:13 +0000

    Fix Bug hero Controller

--- app/Controller/HeroesController.php
+++ app/Controller/HeroesController.php
@@ -15,6 +15,12 @@ class HeroesController extends AppController {
  */
 	public $components = array('Paginator');
 
+    public function beforeFilter()
+	{
+		parent::beforeFilter();
+		$this->Auth->allow('home', 'index', 'view', 'randomTeam', 'randomTeamByStrategy');
+	}
+
 /**
  * index method
  *
@@ -52,8 +58,12 @@ class HeroesController extends AppController {
 		if (!$this->Hero->exists($id)) {
 			throw new NotFoundException(__('Invalid hero'));
 		}
-		$options = array('conditions' => array('Hero.' . $this->Hero->primaryKey => $id));
-		$this->set('hero', $this->Hero->find('first', $options));
+		//$options = array('conditions' => array('Hero.' . $this->Hero->primaryKey => $id));
+        $this->set(array('hero' => $this->Hero->view($id), 'topHeroRoles' => $this->Hero->RolesHero->getTopRolesByHero($id)));
+ /*       	$this->set(array(
+				'hero' => $this->Hero->view($id),
+				'topHeroRoles' => $this->Hero->getTopRoles($id),
+				));*/
 	}
 
 /**
@@ -73,6 +83,7 @@ class HeroesController extends AppController {
 		}
 		$sides = $this->Hero->Side->find('list');
 		$groups = $this->Hero->Group->find('list');
+
 		$this->set(compact('sides', 'groups'));
 	}
 
@@ -123,4 +134,25 @@ class HeroesController extends AppController {
 		}
 		return $this->redirect(array('action' => 'index'));
 	}
+
+    public function home(){
+        $this->set('heroes', $this->Hero->getHeroOrderedByGroups());
+    }
+
+    public function randomTeam(){
+        $heroes = $this->Hero->getRandomTeam();
+        $this->set('randomTeam', $heroes);
+        $this->set('rolesAvg', $this->Hero->RolesHero->getAverageLevelsPerRole($heroes));
+    }
+
+    public function randomTeamByStrategy($strategy_id=null){
+        if($strategy_id==null)
+            $roles = $this->Hero->RolesHero->Role->getRolesByStrategy(1);
+        else
+            $roles = $this->Hero->RolesHero->Role->getRolesByStrategy($strategy_id);
+        $heroes = $this->Hero->RolesHero->getRandomTeamByRoles($roles);
+        $this->set('Strategy', $roles);
+        $this->set('randomTeam', $heroes);
+        $this->set('rolesAvg', $this->Hero->RolesHero->getAverageLevelsPerRole($heroes));
+    }
 }
